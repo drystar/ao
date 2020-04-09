fd2c40325f23457e2b76c3d3d5f0dcd99e79e3fc
drystar <chris.drysdale12@gmail.com>
2020-04-08 23:46:20 -0700

    more app blueprint

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -9,6 +9,38 @@ const HomePage = () => (
           <span className="subtitle">SHOP NOW</span>
         </div>
       </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">JACKETS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">SNEAKERS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">MENS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">WOMANS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">JNR</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
     </div>
   </div>
 );
+
+export default HomePage;
