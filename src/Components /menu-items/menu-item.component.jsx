e0f0e406155994c08634a27c135e2fa18a90f9a4
drystar <chris.drysdale12@gmail.com>
2020-04-11 23:28:14 -0700

    dynamically at styles menu component using template strings

--- src/Components /menu-items/menu-item.component.jsx
+++ src/Components /menu-items/menu-item.component.jsx
@@ -1,9 +1,14 @@
-import React from "./node_modules/react";
+import React from "react";
 
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title }) => (
-  <div className="menu-item">
+const MenuItem = ({ title, imageUrl }) => (
+  <div
+    style={{
+      backgroundImage: `url(${imageUrl})`,
+    }}
+  >
+    className="menu-item">
     <div className="content">
       <h1 className="title">{title}</h1>
       <span className="subtitle">SHOP NOW</span>
