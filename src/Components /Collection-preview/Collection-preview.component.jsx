1f1430182b19ed5f563fc282057326b2c39fac2c
drystar <chris.drysdale12@gmail.com>
2020-04-14 22:41:00 -0700

    create collection item component | import collection item to prevoew to access props

--- src/Components /Collection-preview/Collection-preview.component.jsx
+++ src/Components /Collection-preview/Collection-preview.component.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 
+import CollectionItem from "../Collection-item/Collection-item.component";
+
 import "./Collection-preview.styles.scss";
 
 const CollectionPreview = ({ title, items }) => (
-  <div className="Collection-preview">
+  <div className="collection-preview">
     <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
       {items
         .filter((item, idx) => idx < 4)
-        .map((item) => (
-          <div key>
-            {item.id}>{item.name}
-          </div>
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
         ))}
     </div>
   </div>
