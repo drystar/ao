62019bacc8564850914a3026ed127da4a8ded363
drystar <chris.drysdale12@gmail.com>
2020-04-17 23:05:04 -0700

    add unsubscribeFromAuth to avoid memory leaks

--- src/App.js
+++ src/App.js
@@ -18,12 +18,18 @@ class App extends React.Component {
     };
   }
 
+  unsubscribeFromAuth = null;
+
   componentDidMount() {
-    auth.onAuthStateChanged((user) => {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
       this.setState({ currentUser: user });
 
       console.log(user);
     });
+  }
+
+  componentDidMount() {
+    this.unsubscribeFromAuth();
   }
 
   render() {
