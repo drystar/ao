ff1d09a6df88b57a46d8dfb81833694041d08243
drystar <chris.drysdale12@gmail.com>
2020-04-17 22:39:01 -0700

    convert app to class based to access state

--- src/App.js
+++ src/App.js
@@ -7,18 +7,29 @@ import HomePage from "./Pages/Homepage/homepage.component";
 import ShopPage from "./Pages/Shop/shop.component";
 import Header from "./Components/Header/Header.component";
 import SignInAndSignUp from "./Pages/Sign-in-and-sign-up/Sign-in-and-sign-up.component";
+import { auth } from "./Firebase/firebase.utils";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInAndSignUp} />
-      </switch>
-    </div>
-  );
+class App extends React.Component() {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUp} />
+        </switch>
+      </div>
+    );
+  }
 }
 
 export default App;
