30821fa39e29df8494385d98ebe62056f61050d3
drystar <chris.drysdale12@gmail.com>
2020-04-17 22:13:07 -0700

    configure firebase | initiate google auth

--- src/Firebase/firebase.utils.js
+++ src/Firebase/firebase.utils.js
@@ -15,3 +15,13 @@ const config = {
 };
 
 firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+// Google Auth Utility
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
