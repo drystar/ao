8186fcfe0f7edc0bc29736b9efed84af244272f8
drystar <chris.drysdale12@gmail.com>
2020-04-27 23:59:18 -0700

    create SignUp component

---
+++ src/Components/SignUp/SignUp.component.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import FormInput from "../Form-input/Form-input.component";
+import CustomButton from "../Custom-button/Custom-button.component";
+
+import { auth, createProfileDocument } from "../../Firebase/firebase.utils";
+
+import "./SignUp.styles.scss";
+
+class SignUp extends React.Component {
+  constructor() {
+    super();
+  }
+}
+
+export default SignUp;
