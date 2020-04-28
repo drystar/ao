9a8cf94c7a23c7c91051df2bae4446f323bee007
drystar <chris.drysdale12@gmail.com>
2020-04-28 00:13:22 -0700

    create state | add form input options to sign up

--- src/Components/SignUp/SignUp.component.jsx
+++ src/Components/SignUp/SignUp.component.jsx
@@ -10,6 +10,58 @@ import "./SignUp.styles.scss";
 class SignUp extends React.Component {
   constructor() {
     super();
+
+    this.state = {
+      displayName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    };
+  }
+
+  render() {
+    const { displayName, email, password, confirmPassword } = this.state;
+    return (
+      <div className="sign-up">
+        <h2 className="title">I Do Not Have an Account Yet</h2>
+        <span>Sign Up with Your Email & Password</span>
+        <form className="sign-up-form" onSubmit={this.handleSubmit}>
+          <FormInput
+            type="text"
+            name="displayName"
+            value={displayName}
+            onChange={this.handleChange}
+            label="Display Name"
+            required
+          />
+          <FormInput
+            type="email"
+            name="email"
+            value={email}
+            onChange={this.handleChange}
+            label="Email"
+            required
+          />
+          <FormInput
+            type="password"
+            name="password"
+            value={password}
+            onChange={this.handleChange}
+            label="Password"
+            required
+          />
+          <FormInput
+            type="password"
+            name="confirmPassword"
+            value={confirmPassword}
+            onChange={this.handleChange}
+            label="Confirm Password"
+            required
+          />
+          <CustomButton type="submit"> Sign Up</CustomButton>
+        </form>
+      </div>
+    );
   }
 }
 
