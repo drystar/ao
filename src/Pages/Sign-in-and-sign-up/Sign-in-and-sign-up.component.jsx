5c459271f6a5d5b7d6f51ea6cecdd42884d84f40
drystar <chris.drysdale12@gmail.com>
2020-04-28 01:10:57 -0700

    import SignUp to SignInAndUp to implement and use firebase utils | add styles to SignInAndUp

--- src/Pages/Sign-in-and-sign-up/Sign-in-and-sign-up.component.jsx
+++ src/Pages/Sign-in-and-sign-up/Sign-in-and-sign-up.component.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
-import "./Sign-in-and-sign-up.styles.scss";
+// enable email/password & google authentication on firebase sign-in methods
+
 import SignIn from "../../Components/Signin/SignIn.component";
+import SignUp from "../../Components/SignUp/SignUp.component";
+
+import "./Sign-in-and-sign-up.styles.scss";
 
 const SignInAndSignUpPage = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 );
 
