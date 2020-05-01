c476c428a3e2e93f78af2d953fcf3b856c36ecf8
drystar <chris.drysdale12@gmail.com>
2020-04-30 20:38:03 -0700

    import redirect component to modify sign in route for current user | render [JS invocation]

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
 import "./App.css";
@@ -11,6 +11,7 @@ import SignInAndSignUp from "./Pages/Sign-in-and-sign-up/Sign-in-and-sign-up.com
 import { auth, createUserProfileDocument } from "./Firebase/firebase.utils";
 
 import { setCurrentUser } from "./redux/user/user.actions";
+import SignInAndSignUpPage from "./Pages/Sign-in-and-sign-up/Sign-in-and-sign-up.component";
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -48,15 +49,29 @@ class App extends React.Component {
         <switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignInAndSignUp} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to="/" />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+          />
         </switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
