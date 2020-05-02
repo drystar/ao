8aeaa0a479b04ca03badf47a02dd4da3a10db062
drystar <chris.drysdale12@gmail.com>
2020-05-01 17:38:36 -0700

    add CustomButtton to CartDropdown | add styles to container

--- src/Components/Cart-dropdown/Cart-dropdown.component.jsx
+++ src/Components/Cart-dropdown/Cart-dropdown.component.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
-import "./cart-dropdown.styles.scss";
+import CustomButton from "../Custom-button/Custom-button.component";
+
+import "./Cart-dropdown.styles.scss";
 
 const CartDropdown = () => (
   <div className="cart-dropdown">
     <div className="cart-item" />
+    <CustomButton>Checkout</CustomButton>
   </div>
 );
 
