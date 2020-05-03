146ac42d893fc58f2f87e8ebc8d5d885c91bf7d6
drystar <chris.drysdale12@gmail.com>
2020-05-02 19:08:28 -0700

    create cart action | import action to CartIcon

--- src/Components/Cart-icon/Cart-icon.component.jsx
+++ src/Components/Cart-icon/Cart-icon.component.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import { toggleCartHidden } from "../../redux/cart/cart.action";
 
 import { ReactComponent as ShoppingIcon } from "../../assets/cart.svg";
 
