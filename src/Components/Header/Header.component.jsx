364234e8b11e651668505ba32593b3bce5bffbdf
drystar <chris.drysdale12@gmail.com>
2020-05-02 22:18:16 -0700

    implement toggle fucntion with ternary | destructure nested state properties in mapStateToProps function

--- src/Components/Header/Header.component.jsx
+++ src/Components/Header/Header.component.jsx
@@ -12,7 +12,7 @@ import { ReactComponent as Logo } from "../../assets/ao.svg";
 
 import "./Header.styles.scss";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -35,12 +35,13 @@ const Header = ({ currentUser }) => (
       )}
       <CartIcon />
     </div>
-    <CartDropdown />
+    {hidden ? null : <CartDropdown />}
   </div>
 );
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 
 export default connect(mapStateToProps)(Header);
