af4c9cfb90c4a51d70fe86298d92b04dee5dc475
drystar <chris.drysdale12@gmail.com>
2020-05-03 23:47:49 -0700

    make a new addItem action | integrate redux & import addItem action into Collection-item

--- src/Components/Collection-item/Collection-item.component.jsx
+++ src/Components/Collection-item/Collection-item.component.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import CustomButton from "../Custom-button/Custom-button.component";
+import { addItem } from "../../redux/cart/cart.actions";
 
 import "./Collection-item.styles.scss";
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
+const CollectionItem = ({ id, name, price, imageUrl, addItem }) => (
   <div className="collection-item">
     <div
       className="image"
@@ -20,4 +22,8 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
   </div>
 );
 
-export default CollectionItem;
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
