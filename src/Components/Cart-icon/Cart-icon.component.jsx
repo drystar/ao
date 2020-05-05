f14b4d8d06f700fb2acb4cc47f1367c8682ac391
drystar <chris.drysdale12@gmail.com>
2020-05-05 00:03:59 -0700

    pass itemCount as a prop

--- src/Components/Cart-icon/Cart-icon.component.jsx
+++ src/Components/Cart-icon/Cart-icon.component.jsx
@@ -7,10 +7,10 @@ import { ReactComponent as ShoppingIcon } from "../../assets/cart.svg";
 
 import "./Cart-icon.styles.scss";
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
 
