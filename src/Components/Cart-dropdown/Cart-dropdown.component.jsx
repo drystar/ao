7c8535ae64d0dfa9b89999b16b83a7d9755ad418
drystar <chris.drysdale12@gmail.com>
2020-05-04 23:28:11 -0700

    fix CartDrop cart-items classnName to apply styles

--- src/Components/Cart-dropdown/Cart-dropdown.component.jsx
+++ src/Components/Cart-dropdown/Cart-dropdown.component.jsx
@@ -8,7 +8,7 @@ import "./Cart-dropdown.styles.scss";
 
 const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
-    <div className="cart-item">
+    <div className="cart-items">
       {cartItems.map((cartItem) => (
         <CartItem key={cartItem.id} item={cartItem} />
       ))}
