1b9baa6eb013fb327510c18e689a4ea614cc3ded
drystar <chris.drysdale12@gmail.com>
2020-05-13 21:23:07 -0700

    create shopReduce | import to rootReducer

--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -8,6 +8,7 @@ import storage from "redux-persist/lib/storage";
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
 
 const persistConfig = {
   key: "root",
@@ -21,6 +22,7 @@ const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
   directory: directoryReducer,
+  shop: shopReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
