563338fd44bd22a3162b2a495a6445a245961daf
drystar <chris.drysdale12@gmail.com>
2020-05-13 21:42:43 -0700

    create shop selectors | connect shop component to redux

--- src/Pages/Shop/shop.component.jsx
+++ src/Pages/Shop/shop.component.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
+import { selectCollections } from "../../redux/shop/shop.selectors";
 
 import CollectionPreview from "../../Components/Collection-preview/Collection-preview.component";
 
@@ -10,4 +14,8 @@ const ShopPage = ({ collections }) => (
   </div>
 );
 
-export default ShopPage;
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+});
+
+export default connect(mapStateToProps)(ShopPage);
