eb2482249d16734f2e6995536916e4f03be895ee
drystar <chris.drysdale12@gmail.com>
2020-05-15 14:12:15 -0700

    create nested routing on Shop Page

--- src/Pages/Shop/shop.component.jsx
+++ src/Pages/Shop/shop.component.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Route } from "react-router-dom";
 
 import CollectionOverview from "../../Components/Collections-overview/Collections-overview.component";
 
-const ShopPage = ({ collections }) => (
+const ShopPage = ({ match }) => (
   <div className="shop-page">
-    <CollectionOverview />
+    <Route exact path={`${match.path}`} component={CollectionOverview} />
   </div>
 );
 
