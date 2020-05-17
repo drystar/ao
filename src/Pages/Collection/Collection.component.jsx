7e44406d4a3de8d3581a75a2d61d27b4ba19ace6
drystar <chris.drysdale12@gmail.com>
2020-05-16 23:53:52 -0700

    create new selector to convert obj to array | import update to CollectionPreview

--- src/Pages/Collection/Collection.component.jsx
+++ src/Pages/Collection/Collection.component.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 
-import {
-  selectCollection,
-  selectCollections,
-} from "./../../redux/shop/shop.selectors";
+import { selectCollection } from "./../../redux/shop/shop.selectors";
 
 import CollectionItem from "../../Components/Collection-item/Collection-item.component";
 
