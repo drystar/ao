ab1ebd9c669a515b89efab2b19b0bd4068705be0
drystar <chris.drysdale12@gmail.com>
2020-05-16 23:11:36 -0700

    map over items array and render collection | update styles

--- src/Pages/Collection/Collection.component.jsx
+++ src/Pages/Collection/Collection.component.jsx
@@ -11,10 +11,15 @@ import CollectionItem from "../../Components/Collection-item/Collection-item.com
 import "./Collection.styles.scss";
 
 const CollectionPage = ({ collection }) => {
-  console.log(collection);
+  const { title, items } = collection;
   return (
     <div className="collection-page">
-      <h2> Collection Page </h2>
+      <h2 className="title"> {title} </h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
