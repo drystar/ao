5ec4fd7a547250f889fbc41858011b2b094dc680
drystar <chris.drysdale12@gmail.com>
2020-05-20 20:13:38 -0700

    ....

--- src/Pages/Homepage/Homepage.component.jsx
+++ src/Pages/Homepage/Homepage.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Directory from "../../Components/Directory/Directory.component1";
+import Directory from "../../Components/Directory/Directory.component";
 
 import "./Homepage.styles.scss";
 
