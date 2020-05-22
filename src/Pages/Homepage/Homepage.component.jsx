e311ce98ca3d50540e44d0c7e8f43f7a69ce7b81
drystar <chris.drysdale12@gmail.com>
2020-05-22 11:51:53 -0700

    remove styles sheet

--- src/Pages/Homepage/Homepage.component.jsx
+++ src/Pages/Homepage/Homepage.component.jsx
@@ -2,8 +2,6 @@ import React from "react";
 
 import Directory from "../../Components/Directory/Directory.component";
 
-import "./Homepage.styles.scss";
-
 import { HomePageContainer } from "./Homepage.styles";
 
 const HomePage = () => (
