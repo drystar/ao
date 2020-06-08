157f37941754ba643863b6026d8afca873864af9
drystar <chris.drysdale12@gmail.com>
2020-06-08 16:51:00 -0700

    import & update with compose

--- src/Components/Collections-overview/Collections-overview.container.jsx
+++ src/Components/Collections-overview/Collections-overview.container.jsx
@@ -1,5 +1,6 @@
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
+import { compose } from "redux";
 
 import { selectisCollectionFetching } from "../../redux/shop/shop.selectors";
 import WithSpinner from "../With-spinner/With-spinner.component";
@@ -9,8 +10,16 @@ const mapStateToProps = createStructuredSelector({
   isLoading: selectIsCollectionFetching,
 });
 
-const CollectionsOverviewContainer = connect(mapStateToProps)(
-  WithSpinner(CollectionsOverview)
-);
-
+// const CollectionsOverviewContainer = connect(mapStateToProps)(
+//   WithSpinner(CollectionsOverview)
+// );
 // HOC - Functions will evaulate from the inside out
+
+const CollectionsOverviewContainer = compose(
+  connect(mapStateToProps),
+  WithSpinner
+)(CollectionsOverview);
+
+// compose evaulates from right to left | essentially carries all the functions together
+
+export default CollectionsOverviewContainer;
