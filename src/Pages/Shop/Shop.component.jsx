d2e3754985ba2bccf9c5e2e18f3919017c1ef8ae
drystar <chris.drysdale12@gmail.com>
2020-06-08 17:38:00 -0700

    update shop | clean up

--- src/Pages/Shop/Shop.component.jsx
+++ src/Pages/Shop/Shop.component.jsx
@@ -1,18 +1,11 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
 
 import CollectionsOverviewContainer from "../../Components/Collections-overview/Collections-overview.container";
-import CollectionPage from "../Collection/Collection.component";
+import CollectionPageContainer from "../Collection/Collection.container";
 
 import { fetchCollectionsStartAsync } from "../../redux/shop/shop.actions";
-import { selectIsCollectionsLoaded } from "../../redux/shop/shop.selectors";
-
-import WithSpinner from "../../Components/With-spinner/With-spinner.component";
-
-const CollectionsOverviewWithSpinner = WithSpinner(CollectionOverview);
-const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
   componentDidMount() {
@@ -21,7 +14,7 @@ class ShopPage extends React.Component {
   }
 
   render() {
-    const { match, isCollectionsLoaded } = this.props;
+    const { match } = this.props;
     return (
       <div className="shop-page">
         <Route
@@ -29,24 +22,15 @@ class ShopPage extends React.Component {
           path={`${match.path}`}
           component={CollectionsOverviewContainer}
         />
-        )} />
         <Route
           path={`${match.path}/:collectionId`}
-          render={(props) => (
-            <CollectionPageWithSpinner
-              isLoading={!isCollectionsLoaded}
-              {...props}
-            />
-          )}
+          component={CollectionPageContainer}
         />
+        )} />
       </div>
     );
   }
 }
-
-const mapStateToProps = createStructuredSelector({
-  isCollectionsLoaded: selectIsCollectionsLoaded,
-});
 
 const mapDispatchToProps = (dispatch) => ({
   fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync()),
