62ee5baf7355dc16cf35f63a963255cc5ed39bfb
drystar <chris.drysdale12@gmail.com>
2020-06-09 13:28:05 -0700

    fix typo

--- src/redux/shop/shop.saga.js
+++
@@ -1,7 +0,0 @@
-import { takeEvery } from "redux-saga/effects";
-
-import ShopActionTypes from "./shop.types";
-
-export function* fetchCollectionsStart(){
-  yeild takeEvery(ShopActionTypes.FETCH_COLLECTIONS_START, )
-}
--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -3,9 +3,12 @@ import { takeEvery } from "redux-saga/effects";
 import ShopActionTypes from "./shop.types";
 
 export function* fetchCollectionsAsync() {
-  yield console.log("I am fired")
+  yield console.log("I am fired");
 }
 
 export function* fetchCollectionsStart() {
-  yeild takeEvery(ShopActionTypes.FETCH_COLLECTIONS_START, fetchCollectionsAsync)
+  yield takeEvery(
+    ShopActionTypes.FETCH_COLLECTIONS_START,
+    fetchCollectionsAsync
+  );
 }
