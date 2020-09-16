bcf74f14db9b92a79a6f47cf7d1bfb2b748977c1
drystar <chris.drysdale12@gmail.com>
2020-09-16 15:15:42 -0700

    begin user auth refactor to promise orientated | frame user actions and types

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -6,3 +6,32 @@ export const setCurrentUser = (user) => ({
   type: userActionTypes.SET_CURRENT_USER,
   payload: user,
 });
+
+export const googleSignInStart = () => ({
+  type: UserActionTypes.GOOGLE_SIGN_IN_START,
+});
+
+export const googleSignInSuccess = (user) => ({
+  type: UserActionTypes.GOOGLE_SIGN_IN_SUCCESS,
+  payload: user,
+});
+
+export const googleSignInFailure = (error) => ({
+  type: UserActionTypes.GOOGLE_SIGN_IN_FAILURE,
+  payload: error,
+});
+
+export const emailSignInStart = (emailAndPassword) => ({
+  type: UserActionTypes.EMAIL_SIGN_IN_START,
+  payload: emailAndPassword,
+});
+
+export const emailSignInStart = (user) => ({
+  type: UserActionTypes.EMAIL_SIGN_IN_SUCCESS,
+  payload: user,
+});
+
+export const emailSignInStart = (error) => ({
+  type: UserActionTypes.EMAIL_SIGN_IN_FAILURE,
+  payload: error,
+});
