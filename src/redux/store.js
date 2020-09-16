8d9264187dd2f657088a155d8e685b398a38cbf1
drystar <chris.drysdale12@gmail.com>
2020-09-15 23:05:00 -0700

    switch over take | create a root saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,9 +4,8 @@ import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 import createSagaMiddleware from "redux-saga";
 
-import { fetchCollectionStart, fetchCollectionsStart } from "./shop/shop.sagas";
-
 import rootReducer from "./root.reducer";
+import rootSaga from "./root-saga";
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -20,7 +19,7 @@ if (process.env.NODE_ENV === "development") {
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 // individual sagas will run from here
-sagaMiddleware.run(fetchCollectionsStart);
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
 
