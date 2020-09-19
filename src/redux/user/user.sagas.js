bc07a3bace6649d165d441dfbb3358d55651aab4
drystar <chris.drysdale12@gmail.com>
2020-09-18 21:34:54 -0700

    create resusable generator fucntion getSnapshotFromUserAuth

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -2,10 +2,7 @@ import { takeLatest, put, all, call } from "redux-saga/effects";
 
 import userActionTypes from "./user.types";
 
-import {
-  signInSuccess,
-  signInFailure,
-} from "./user.actions";
+import { signInSuccess, signInFailure } from "./user.actions";
 
 import {
   auth,
@@ -13,15 +10,21 @@ import {
   createUserProfileDocument,
 } from "../../Firebase/firebase.utils";
 
+export function* getSnapshotFromUserAuth(userAuth) {
+  try {
+    const userRef = yield call(createUserProfileDocument, userAuth);
+    console.log(userRef);
+    const userSnapShot = yield userRef.get();
+    yield put(signInSuccess({ id: userSnapShot.id, ...userSnapShot.data() }));
+  } catch (error) {
+    yield put(signInFailure(error));
+  }
+}
+
 export function* signInWithGoogle() {
   try {
     const { user } = yield auth.signInWithPopup(googleProvider);
-    const userRef = yield call(createUserProfileDocument, user);
-    console.log(userRef);
-    const userSnapShot = yield userRef.get();
-    yield put(
-      signInSuccess({ id: userSnapShot.id, ...userSnapShot.data() })
-    );
+    yield getSnapshotFromUserAuth(user);
   } catch (error) {
     yield put(signInFailure(error));
   }
@@ -31,17 +34,14 @@ export function* onGoogleSignInStart() {
   yield takeLatest(userActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle);
 }
 
-export function* signInWitEmail({payload: { email, password }})
- try {
+export function* signInWitEmail({ payload: { email, password } }) {
+  try {
     const { user } = yield auth.signInWithEmailAndPassword(email, password);
-    const userRef = yield call(createUserProfileDocument, user);
-    const userSnapShot = yield userRef.get();
-    yield put(
-      signInSuccess({ id: userSnapShot.id, ...userSnapShot.data() })
-    );
+    yield getSnapshotFromUserAuth(user);
   } catch (error) {
     yield put(signInFailure(error));
   }
+}
 
 export function* OnEmailSignInStart() {
   yield takeLatest(userActionTypes.EMAIL_SIGN_IN_START, signInWitEmail);
