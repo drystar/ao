baf7120b59df2b1de262df3ecf3d9fbc87f468ab
drystar <chris.drysdale12@gmail.com>
2020-12-04 14:26:57 -0800

    update ShopPage to functional | import useEffect

--- src/Pages/Shop/Shop.component.jsx
+++ src/Pages/Shop/Shop.component.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route } from "react-router-dom";
 import { connect } from "react-redux";
 
@@ -7,13 +7,12 @@ import CollectionPageContainer from "../Collection/Collection.container";
 
 import { fetchCollectionsStart } from "../../redux/shop/shop.actions";
 
-class ShopPage extends React.Component {
+const ShopPage = () => {
   componentDidMount() {
     const { fetchCollectionsStart } = this.props;
     fetchCollectionsStart();
   }
 
-  render() {
     const { match } = this.props;
     return (
       <div className="shop-page">
@@ -29,7 +28,6 @@ class ShopPage extends React.Component {
       </div>
     );
   }
-}
 
 const mapDispatchToProps = (dispatch) => ({
   fetchCollectionsStart: () => dispatch(fetchCollectionsStart()),
