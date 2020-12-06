2f6432af5258a09b3c9d02228c1df8c664ace51a
drystar <chris.drysdale12@gmail.com>
2020-12-05 21:41:48 -0800

    add code to serve from final production build

--- server.js
+++ server.js
@@ -12,3 +12,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
