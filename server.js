fae80826b92ff8cc6e2e8eb0771d5d791ace8d31
drystar <chris.drysdale12@gmail.com>
2020-12-05 22:09:10 -0800

    add client success/ error response for stripe charging

--- server.js
+++ server.js
@@ -34,4 +34,12 @@ app.post("/payment", (req, res) => {
     amount: req.body.amount,
     currency: "CAD",
   };
+
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr });
+    } else {
+      res.status(200).send({ success: stripeRes });
+    }
+  });
 });
