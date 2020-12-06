7021672ba5bc508492bec32b347faea25070fb6e
drystar <chris.drysdale12@gmail.com>
2020-12-05 21:05:19 -0800

    create new client folder and transfer front end

---
+++ client/build/precache-manifest.0c5eaea47c2e11a091dd3a09ec419a5b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d15f5a7f61f562719448e06497af597",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef64c4210480871c7850",
+    "url": "/static/css/main.e6040b56.chunk.css"
+  },
+  {
+    "revision": "07856501845983e1cfbb",
+    "url": "/static/js/2.5b854efc.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/static/js/2.5b854efc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef64c4210480871c7850",
+    "url": "/static/js/main.6c06ff9e.chunk.js"
+  },
+  {
+    "revision": "754db299154c7dc977ef",
+    "url": "/static/js/runtime-main.cfaacd98.js"
+  },
+  {
+    "revision": "9d1f8621ba09bcd2bb854d4ab14584d5",
+    "url": "/static/media/ao.9d1f8621.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/cart.30f07071.svg"
+  }
+]);
