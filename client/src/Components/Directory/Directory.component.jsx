1872e2b3fcd8818b7be250f626a28b353782cda7
drystar <chris.drysdale12@gmail.com>
2020-12-07 13:04:59 -0800

    create styles | convert Directory to styled component

--- client/src/Components/Directory/Directory.component.jsx
+++ client/src/Components/Directory/Directory.component.jsx
@@ -1,23 +1,23 @@
-import React from "react";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
-import { selectDirectorySections } from "../../redux/directory/directory.selectors";
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 
-import "./Directory.styles.scss";
+import MenuItem from '../menu-item/menu-item.component';
 
-import MenuItem from "../Menu-items/Menu-item.component";
+import { DirectoryMenuContainer } from './directory.styles';
 
 const Directory = ({ sections }) => (
-  <div className="directory-menu">
+  <DirectoryMenuContainer>
     {sections.map(({ id, ...otherSectionProps }) => (
       <MenuItem key={id} {...otherSectionProps} />
     ))}
-  </div>
+  </DirectoryMenuContainer>
 );
 
-const mapstateToProps = createStructuredSelector({
-  sections: selectDirectorySections,
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
 });
 
-export default connect(mapstateToProps)(Directory);
+export default connect(mapStateToProps)(Directory);
