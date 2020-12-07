b3cd87d05a3a10328de81b425267578ce9f1a8b6
drystar <chris.drysdale12@gmail.com>
2020-12-06 21:10:03 -0800

    access context with function

--- client/src/Pages/Collection/Collection.component.jsx
+++ client/src/Pages/Collection/Collection.component.jsx
@@ -8,18 +8,25 @@ import CollectionItem from "../../Components/Collection-item/Collection-item.com
 
 import "./Collection.styles.scss";
 
-const CollectionPage = ({ collection }) => {
-  const { title, items } = collection;
+const CollectionPage = ({ match }) => {
   return (
     <CollectionsContext.Consumer>
-    <div className="collection-page">
-      <h2 className="title"> {title} </h2>
-      <div className="items">
-        {items.map((item) => (
-          <CollectionItem key={item.id} item={item} />
-        ))}
-      </div>
-    </div>
+    {
+      collections => {
+        const collection = collections[match.params.collectionId]
+        const { title, items } = collection;
+        return (     
+           <div className="collection-page">
+            <h2 className="title"> {title} </h2>
+            <div className="items">
+              {items.map((item) => (
+                 <CollectionItem key={item.id} item={item} />
+              ))}
+             </div>
+           </div>
+          )
+      }
+    }
     </CollectionsContext.Consumer>
   );
 };
